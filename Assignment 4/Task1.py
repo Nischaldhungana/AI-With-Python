15b7b4992d243d5480a82be28d903d1f78f955c3
Nischaldhungana <dhungananischal371@gmail.com>
2025-09-29 16:32:21 +0545

    I have completed the assignment4

--- Assignment 4/Task1.py
+++ Assignment 4/Task1.py
@@ -4,17 +4,16 @@ import matplotlib.pyplot as plt
 n_values = [500, 1000, 2000, 5000, 10000, 15000, 20000, 50000, 100000]
 
 for n in n_values:
-    d1 = np.random.randint(1, 7, size=n)
-    d2 = np.random.randint(1, 7, size=n)
-    s = d1 + d2
-    h, edges = np.histogram(s, range(2, 14))
+    dice1 = np.random.randint(1, 7, size=n)
+    dice2 = np.random.randint(1, 7, size=n)
 
-    plt.figure(figsize=(7,4))
-    plt.bar(edges[:-1], h / n, width=0.9, align='center')
-    plt.xticks(edges[:-1])
+    sums = dice1 + dice2
+    h, h2 = np.histogram(sums, range(2, 14))
+
+
+    plt.bar(h2[:-1], h / n)
+
     plt.xlabel("Sum of two dice")
     plt.ylabel("Relative frequency")
     plt.title(f"Sum of two dice (n = {n})")
-    plt.ylim(0, 0.2)
-    plt.tight_layout()
     plt.show()
